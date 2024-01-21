60f25809508f1c9f1d91996458c3aaa5f4d59e1e
khaledCSE <khaledcse10@gmail.com>
2024-01-21 15:27:59 +0000

    Blade basics

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -66,29 +63,23 @@
     </style>
 </head>
 
-<body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizzas
         </div>
+        <p>{{ $type }} - {{ $base }} - ${{ $price }}</p>
+
+        @if ($price > 15)
+            <p>This pizza is expensive!</p>
+        @elseif($price < 5)
+            <p>This pizza is cheap!</p>
+        @else
+            <p>This pizza is normally priced</p>
         @endif
 
-        <div class="content">
-            <div class="title m-b-md">
-                Pizza House<br />
-                The North's Best Pizzas
-            </div>
-        </div>
+        @unless ($base == 'Cheesy Crust')
+            <p>You don't have a Cheesy Crust</p>
+        @endunless
     </div>
-</body>
-
-</html>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    $pizza = [
+        'type' => 'Hawaiian',
+        'base' => 'Garlic Crust',
+        'price' => 10,
+    ];
+    return view('pizzas', $pizza);
+});
