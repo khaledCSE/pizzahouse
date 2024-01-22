74ba4739c80a0d1d5112f07ba77441c58e0a5b30
khaledCSE <khaledcse10@gmail.com>
2024-01-22 04:03:31 +0000

    Named routes

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Route::get('/', function () {
 });
 
 // pizza routes
-Route::get('/pizzas', [PizzaController::class, 'index'])->middleware('auth');
-Route::post('/pizzas', [PizzaController::class, 'store']);
-Route::get('/pizzas/create', [PizzaController::class, 'create']);
-Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->middleware('auth');
-Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy'])->middleware('auth');
+Route::get('/pizzas', [PizzaController::class, 'index'])->name('pizzas.index')->middleware('auth');
+Route::post('/pizzas', [PizzaController::class, 'store'])->name('pizzas.store');
+Route::get('/pizzas/create', [PizzaController::class, 'create'])->name('pizzas.create');
+Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->name('pizzas.show')->middleware('auth');
+Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy'])->name('pizzas.destroy')->middleware('auth');
 
 Auth::routes([
     'register' => false
