afc8582de5d663ca8483789a59ed827c128191a3
khaledCSE <khaledcse10@gmail.com>
2024-01-22 02:49:46 +0000

    Authentication added

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="wrapper pizza-index">
+        <h1>Pizza Orders</h1>
         @foreach ($pizzas as $pizza)
             <div class="pizza-item">
                 <img src="/img/pizza.png" alt="pizza icon">
